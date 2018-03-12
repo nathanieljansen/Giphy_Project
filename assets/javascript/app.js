a8db306cb46602afd069fab46b71c608a3776f41
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-11 21:38:38 -0600

    working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -72,19 +72,19 @@ $(function () {
   createButtons();
 
 
-  $(document).on("click", ".work",function () {
-    console.log("working")
-    var state = $(this).attr("data-state");
+  // $(document).on("click", ".work",function () {
+  //   console.log("working")
+  //   var state = $(this).attr("data-state");
     
-      if (state == "still") {
-        $(this).attr("src", $(this).data("move"))
-        $(this).attr("data-state", "move");
-      }
-      else {
-        $(this).attr("src", $(this).data("still"))
-        $(this).attr("data-state", "still");
-      }
-    })
+  //     if (state == "still") {
+  //       $(this).attr("src", $(this).data("move"))
+  //       $(this).attr("data-state", "move");
+  //     }
+  //     else {
+  //       $(this).attr("src", $(this).data("still"))
+  //       $(this).attr("data-state", "still");
+  //     }
+  //   })
 
 });
 
